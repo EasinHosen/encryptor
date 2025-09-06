e6ff59545026d42b1a83d46d89d79491a692f15d
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2025-09-06 14:55:36 +0600

    feat: app color

---
+++ lib/view/styles/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color colorPrimary = Color(0xFF1565C0);
+  static const Color colorSecondary = Colors.tealAccent;
+
+  static const Color colorWhite = Colors.white;
+}
