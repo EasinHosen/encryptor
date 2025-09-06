063e6f423636748540d6ec196de814c33b1b40fd
Easin Md. Hosen <easin.hosen@bengalmobileqa.com>
2025-09-06 19:48:55 +0600

    feat: ceaser encryption added

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -2,4 +2,18 @@ import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   static HomeController get to => Get.find();
+
+  RxString ceaserEncryptedText = ''.obs;
+
+  String encryptCaesar(String input, int shift) {
+    return input.split('').map((char) {
+      if (char.contains(RegExp(r'[A-Za-z]'))) {
+        final base = char.codeUnitAt(0) >= 97 ? 97 : 65;
+        return String.fromCharCode(
+          ((char.codeUnitAt(0) - base + shift) % 26) + base,
+        );
+      }
+      return char;
+    }).join();
+  }
 }
